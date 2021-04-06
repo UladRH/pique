4f1918f4c42a7fef5d6c4a8948e61bf79846b70f
Uladzislau Hlebovich <uladzislau@sviet.me>
2021-04-07 01:02:34 +0300

    Update PqUser decorator key argument support

--- backend/src/api/shared/decorators/user.decorator.ts
+++ backend/src/api/shared/decorators/user.decorator.ts
@@ -1,6 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const PqUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user;
-});
+import type { User } from '../../auth/entities/user.entity';
+
+export const PqUser = createParamDecorator(
+  <K extends keyof User>(data: K, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+
+    return data ? user?.[data] : user;
+  },
+);
