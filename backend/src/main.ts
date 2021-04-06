829bf33b289d86dd0b1bb5946da27681c2bba650
Uladzislau Hlebovich <uladzislau@sviet.me>
2021-04-06 22:01:34 +0300

    Add cookie session

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { ClassSerializerInterceptor, HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieSession from 'cookie-session';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      name: 'PHPSESSID',
+      sameSite: 'strict',
+      keys: ['secret'], // FIXME
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    }),
+  );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
