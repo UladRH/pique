fe2f87fa06e528c99f990440cb3b86d819e6e341
Uladzislau Hlebovich <uladzislau@sviet.me>
2021-07-04 22:40:18 +0300

    fix(frontend): app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthFacade } from './auth/+state/auth.facade';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private authFacade: AuthFacade) {}
-
-  ngOnInit(): void {
-    this.authFacade.getCurrentUser();
-  }
+  ngOnInit(): void {}
 }
