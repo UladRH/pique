31bd4388973228125312573c577710b9a0ab96e0
Uladzislau Hlebovich <uladzislau@sviet.me>
2021-07-12 02:02:56 +0300

    fix(backend/profile): UpdateProfileDto fields should be optional

--- backend/src/api/profiles/dto/update-profile.dto.ts
+++ backend/src/api/profiles/dto/update-profile.dto.ts
@@ -1,11 +1,13 @@
-import { MaxLength } from 'class-validator';
+import { IsOptional, MaxLength } from 'class-validator';
 
 export class UpdateProfileDto {
   // @example "User Name"
   @MaxLength(40)
+  @IsOptional()
   displayName?: string;
 
   // @example "Hello, World!"
   @MaxLength(1000)
+  @IsOptional()
   bio?: string;
 }
