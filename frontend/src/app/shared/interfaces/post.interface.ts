b988b6fdc0e430d950cc91d1c6e6f5d42d7a70cc
Uladzislau Hlebovich <uladzislau@sviet.me>
2021-07-13 16:34:14 +0300

    feat(frontend/post): post markup

--- frontend/src/app/shared/interfaces/post.interface.ts
+++ frontend/src/app/shared/interfaces/post.interface.ts
@@ -3,10 +3,14 @@ import { Profile } from './profile.interface';
 export interface Post {
   id: string;
   content: string;
-  mediaAttachments: any[];
+  mediaAttachments: MediaAttachment[];
   createdAt: string;
   profileId: string;
   profile?: Profile;
   likesCount: number;
   liked: boolean;
+}
+
+export interface MediaAttachment {
+  fileUri: string;
 }
