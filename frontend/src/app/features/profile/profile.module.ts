bd0cd41a64664c6015d903eb4735192e159c5143
Uladzislau Hlebovich <uladzislau@sviet.me>
2021-07-28 14:56:09 +0300

    feat(frontend/profile): profile-follow-button component

--- frontend/src/app/features/profile/profile.module.ts
+++ frontend/src/app/features/profile/profile.module.ts
@@ -7,12 +7,13 @@ import { StoreModule } from '@ngrx/store';
 import { AuthModule } from '../auth/auth.module';
 import { ProfileService } from './profile.service';
 import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
+import { ProfileFollowButtonComponent } from './containers/profile-follow-button.component';
 import { ProfileSectionComponent } from './containers/profile-section.component';
 import { ProfileEffects } from './state/profile.effects';
 import * as fromProfile from './state/profile.reducer';
 
 @NgModule({
-  declarations: [ProfileDetailsComponent, ProfileSectionComponent],
+  declarations: [ProfileDetailsComponent, ProfileSectionComponent, ProfileFollowButtonComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -21,6 +22,6 @@ import * as fromProfile from './state/profile.reducer';
     AuthModule,
   ],
   providers: [ProfileService],
-  exports: [ProfileSectionComponent],
+  exports: [ProfileSectionComponent, ProfileFollowButtonComponent],
 })
 export class ProfileModule {}
